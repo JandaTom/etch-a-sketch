94c508a9b7fb5663b93ce4613d87d39555f22fc7
JandaTom <122153993+JandaTom@users.noreply.github.com>
2024-02-07 13:04:58 +0100

    remove borders from css, clean up js committed
    removed border attribute from css
    cleaned up js code a little bit by moving inactive functions to the
    bottom

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+/*Potential improvements to implement: 
+1. Verify user input
+2. Add reset button (and implement reset function)
+*/ 
+
 let rowSize = 20;
 let collumnSize = 20;
 let totalValue = rowSize * collumnSize;
 const containerWidth = 600;
 const containerHeight = 600;
+
 // Size of filllerSize is -2 because of the 1px borders on sides, otherwise it overflows
-let fillerSize = containerWidth / rowSize - 2
+let fillerSize = containerWidth / rowSize
 
 let container = document.querySelector('.container');
 let promptButton = document.querySelector('button');
@@ -22,7 +28,42 @@ promptButton.addEventListener('click', () => {
     progressiveDarkening();
 });
 
-/* Random colors on hover (extra points 1)
+// Each interaction makes the fields 10% darker - bonus exercise 2 
+function progressiveDarkening() {
+    for(let i = 0; i < totalValue; i++) {
+        let fill = document.createElement('div');
+        fill.classList.add("fill");
+        fill.style.height = `${fillerSize}px`;
+        fill.style.width = `${fillerSize}px`;
+        let red = 255;
+        let green = 255;
+        let yellow = 255;
+        fill.addEventListener('mouseenter', () => {
+            red -= 25.5;
+            green -= 25.5;
+            yellow -= 25.5;
+            fill.style.backgroundColor = `rgb(${red}, ${green}, ${yellow})`
+
+        });
+        container.appendChild(fill);
+    };
+};
+
+// Node removal function using querySelectorAll and forEach method
+function removeFields() {
+    let nodeRemoval = document.querySelectorAll('.fill');
+    nodeRemoval.forEach((node) => {
+        container.removeChild(node);
+    });
+};
+
+function colorRandomizer() {
+   let number = Math.floor(Math.random()* 256);
+   return number;
+};
+
+
+/* Random colors on hover - bonus exercise 1
 function createField() {
     for(let i = 0; i < totalValue; i++) {
         let fill = document.createElement('div');
@@ -42,26 +83,6 @@ function createField() {
     };
 }; */
 
-function progressiveDarkening() {
-    for(let i = 0; i < totalValue; i++) {
-        let fill = document.createElement('div');
-        fill.classList.add("fill");
-        fill.style.height = `${fillerSize}px`;
-        fill.style.width = `${fillerSize}px`;
-        let red = 255;
-        let green = 255;
-        let yellow = 255;
-        fill.addEventListener('mouseenter', () => {
-            red -= 25.5;
-            green -= 25.5;
-            yellow -= 25.5;
-            fill.style.backgroundColor = `rgb(${red}, ${green}, ${yellow})`
-
-        });
-        container.appendChild(fill);
-    };
-}
-
 /* Node removal function using forloop
 function removeFields() {
     for(let i = 0; i < totalValue; i++) {
@@ -71,16 +92,3 @@ function removeFields() {
 
 
 }*/
-
-// Node removal function using querySelectorAll and forEach method
-function removeFields() {
-    let nodeRemoval = document.querySelectorAll('.fill');
-    nodeRemoval.forEach((node) => {
-        container.removeChild(node);
-    });
-}
-
-function colorRandomizer() {
-   let number = Math.floor(Math.random()* 256);
-   return number;
-}
