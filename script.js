85c771d1f63d15537e22e5696c3446086a3ae477
JandaTom <122153993+JandaTom@users.noreply.github.com>
2024-02-07 10:36:35 +0100

    Button allows changing number of fields
    Added function to the button that replaces (deletes and re-creates)
    a new amount of fields based on user input.

--- script.js
+++ script.js
@@ -1,33 +1,51 @@
-let rowSize = 40;
-let collumnSize = 40;
-const totalValue = rowSize * collumnSize;
+let rowSize = 20;
+let collumnSize = 20;
+let totalValue = rowSize * collumnSize;
 const containerWidth = 600;
 const containerHeight = 600;
+// Size of filllerSize is -2 because of the 1px borders on sides, otherwise it overflows
+let fillerSize = containerWidth / rowSize - 2
 
 //prompt('Hello user!');
 let container = document.querySelector('.container');
 let promptButton = document.querySelector('button');
 
+createField();
+
 
 promptButton.addEventListener('click', () => {
-    size = prompt('Define row size (integer from 1 to 100).');
-})
+    removeFields();
+    size = parseInt(prompt('Define row size (integer from 1 to 100).'));
+    rowSize = size;
+    collumnSize = size;
+    totalValue = rowSize * collumnSize
+    fillerSize = containerWidth / rowSize - 2
+    createField();
+});
+
+function createField() {
+    for(let i = 0; i < totalValue; i++) {
+        let fill = document.createElement('div');
+        fill.classList.add("fill");
+        fill.style.height = `${fillerSize}px`;
+        fill.style.width = `${fillerSize}px`;
+        fill.addEventListener('mouseenter', () => {
+            fill.style.backgroundColor = 'blue';
+        });
+        fill.addEventListener('mouseleave', () => {
+            fill.style.backgroundColor = 'white';
+        });
+        container.appendChild(fill);
+    };
+}
 
 
-// Size of filllerSize is -2 because of the 1px borders on sides, otherwise it overflows
-let fillerSize = containerWidth / rowSize - 2
+
+function removeFields() {
+    for(let i = 0; i < totalValue; i++) {
+        let nodeRemoval = document.querySelector('.fill');
+        container.removeChild(nodeRemoval);
+    };
 
 
-for(let i = 0; i < totalValue; i++) {
-    let fill = document.createElement('div');
-    fill.classList.add("fill");
-    fill.style.height = `${fillerSize}px`;
-    fill.style.width = `${fillerSize}px`;
-    fill.addEventListener('mouseenter', () => {
-        fill.style.backgroundColor = 'blue';
-    });
-    fill.addEventListener('mouseleave', () => {
-        fill.style.backgroundColor = 'white';
-    });
-    container.appendChild(fill);
-};
+}
